781d79181806dec039f5198c40016a95e39cd9a1
ANTONIO ALBERTO WOWTILLA E SILVA GALD <ANTONIO9661@DIGITALCOLLEGE.LOCAL>
2024-10-10 21:27:56 -0300

    formulario com css

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,60 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className='form-pessoa'>
+        <h2>Cadastro de Alunos</h2>
+        <form className="row g-3" action="">
+          <div className='col-md-9'>
+            <label htmlFor="nomeCompleto" className='form-label'>Nome Completo</label>
+            <input type="text" className='form-control' id='nomeCompleto' placeholder="Seu nome completo" />
+          </div>
+          <div className='col-md-3'>
+            <label htmlFor="dataNasc" className='form-label'>Data Nasc.</label>
+            <input type="date" className='form-control' id='dataNasc' />
+          </div>
+          <div className='col-md-3'>
+            <label htmlFor="cep" className='form-label'>CEP</label>
+            <input type="text" className='form-control' id='cep' placeholder="Seu CEP" />
+          </div>
+          <div className='col-md-8'>
+            <label htmlFor="endereco" className='form-label'>Endereço</label>
+            <input type="text" className='form-control' id='endereco' placeholder="Seu endereço" />
+          </div>
+          <div className='col-md-1'>
+            <label htmlFor="numero" className='form-label'>Número</label>
+            <input type="text" className='form-control' id='numero' placeholder="Número" />
+          </div>
+          <div className='col-md-6'>
+            <label htmlFor="cidade" className='form-label'>Cidade</label>
+            <select id="cidade" className="form-select">
+              <option selected>Sua cidade</option>
+              <option>...</option>
+            </select>
+          </div>
+          <div className='col-md-2'>
+            <label htmlFor="uf" className='form-label'>UF</label>
+            <select id="uf" className="form-select">
+              <option selected>Seu estado</option>
+              <option>...</option>
+            </select>
+          </div>
+          <div className='col-md-4'>
+            <label htmlFor="complemento" className='form-label'>Complemento</label>
+            <input type="text" className='form-control' id='uf' placeholder="Seu complemento" />
+          </div>
+          <div className="col-md-8">
+            <label htmlFor="email" className='form-label'>E-mail</label>
+            <input type="email" className='form-control' id='inputEmail4' />
+          </div>
+          <div className="col-md-4">
+            <label htmlFor="senha" className='form-label'>Senha</label>
+            <input type="password" className='form-control' id='inputPassword4' />
+          </div>
+          <div className="col-12 form-label">
+            <button type="submit" className="btn btn-danger">Cadastrar</button>
+          </div>
+        </form>
+      </main>
     </>
   )
 }
